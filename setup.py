05c6a2bd6e76588681d0479f1d8134bbb81f2bcf
Iliya Malecki <iliyamalecki@gmail.com>
2022-07-20 00:56:19 +0300

    first crack at a python package

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+from benevolent_boilerplate import __version__
+
+setup(
+    name='my_pip_package',
+    version=__version__,
+
+    url='https://github.com/iliya-malecki/benevolent_boilerplate',
+    author='Iliya Malecki',
+    author_email='iliyamalecki@gmail.com',
+    
+    packages=find_packages(),
+    python_requires='>=3.6',
+    install_requires=[
+        'plotly == 5.9.0',
+        'pandas >= 1.0',
+        'numpy >= 1.20',
+        'itertools',
+        'scikit-learn >= 1.0',
+    ],
+)
